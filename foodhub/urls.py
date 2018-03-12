44046a0e54ed057030bd929d00f5eaf59d671399
mac <mac@macs-MacBook-Air.local>
2018-03-12 17:38:31 +0300

    hey

---
+++ foodhub/urls.py
@@ -0,0 +1,38 @@
+"""foodhub URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from restaurants import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('detail/<int:my_id>/', views.detail, name="detail"),
+    path('list/', views.list, name="list"),
+    path('form/', views.form, name="form"),
+    path('update/<int:res_id>', views.update, name="update"),
+    path('delete/<int:res_id>', views.delete, name="delete"),
+    path('register/', views.register, name="register"),
+    path('login/', views.user_login, name="login"),
+    path('logout/', views.user_logout, name="logout"),
+    path('create_item/<int:res_id>/', views.create_item, name="create_item"),
+    path('like/<int:restaraunt_id>/', views.like, name='like-button'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ restaurants/views.py
@@ -0,0 +1,156 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Restaurant, Item, FavourateRestaurant
+from .forms import RestaurantForm, RegisterUserModel, LoginForm, ItemForm
+from django.contrib.auth import authenticate, login ,logout
+
+def list(request):
+    my_restaurants_list={
+    'restaurants': Restaurant.objects.all(),
+    # [
+    #   {
+    #   'name':'Rehams Restaurant',
+    #   'description':'This is my restaurant!',
+    #   'openingtime':'09:00AM',
+    #   'closingtime':'10:00PM',
+    #   },
+    #   {
+    #   'name':'Rainy Days',
+    #   'description':'Yummyyy!',
+    #   'openingtime':'09:00AM',
+    #   'closingtime':'10:00PM',
+    #   },
+    #   {
+    #   'name':'The Strong Ones',
+    #   'description':'For Body Builders!',
+    #   'openingtime':'09:00AM',
+    #   'closingtime':'10:00PM',
+    #   },
+    #   {
+    #   'name':'Vegans',
+    #   'description':'For Vegans!',
+    #   'openingtime':'09:00AM',
+    #   'closingtime':'10:00PM',
+    #   },
+    # ]
+    }
+    return render(request,'list.html',my_restaurants_list)
+
+def detail(request, my_id):
+    restaurant = Restaurant.objects.get(id=my_id)
+    items = Item.objects.filter(restaurant=restaurant)
+    context={
+        'restaurant': restaurant,
+        'items' : items,
+    }
+    return render(request,'detail.html',context)
+
+def form(request):
+    my_form = RestaurantForm()
+    if request.method == 'POST':
+        my_form = RestaurantForm(request.POST, request.FILES or None)
+        if my_form.is_valid():
+            my_form.save()
+            return redirect('list')
+    context={
+        'my_form': my_form,
+    }
+    return render(request,'my_form.html',context)
+
+def create_item(request,res_id):
+    my_item = ItemForm()
+    resobj = Restaurant.objects.get(id=res_id)
+    if request.method == 'POST':
+        my_item = ItemForm(request.POST)
+        if my_item.is_valid():
+            my_item = my_item.save(commit=False)
+            my_item.restaurant = resobj
+            my_item.save()
+            return redirect('detail',my_id=resobj.id)
+    context={
+        'my_item': my_item,
+        'resobj' : resobj,
+    }
+    return render(request,'create_item.html',context)
+
+def update(request, res_id):
+    resobj = Restaurant.objects.get(id=res_id)
+    my_form = RestaurantForm(instance=resobj)
+    if request.method == 'POST':
+        my_form = RestaurantForm(request.POST, request.FILES or None, instance=resobj)
+        if my_form.is_valid():
+            my_form.save()
+            return redirect('detail',my_id=resobj.id)
+    context={
+        'my_form': my_form,
+        'resobj' :resobj,
+    }
+    return render(request,'update_form.html',context)
+
+def delete(request,res_id):
+    Restaurant.objects.get(id=res_id).delete()
+    return redirect('list')
+
+def register(request):
+    my_form = RegisterUserModel()
+    if request.method == 'POST':
+        my_form = RegisterUserModel(request.POST)
+        if my_form.is_valid():
+            person = my_form.save(commit=False)
+            person.set_password(person.password)
+            person.save()
+            username = my_form.cleaned_data['username']
+            password = my_form.cleaned_data['password']
+            authen = authenticate(username=username,password=password)
+            if authen is not None:
+                login(request, authen)
+                return redirect('list')
+    context={
+        'my_form': my_form,
+    }
+    return render(request,'register.html',context)
+
+def user_login(request):
+    my_form = LoginForm()
+    if request.method == 'POST':
+        my_form = LoginForm(request.POST)
+        if my_form.is_valid():
+            username = my_form.cleaned_data['username']
+            password = my_form.cleaned_data['password']
+            authen = authenticate(username=username,password=password)
+            if authen is not None:
+                login(request, authen)
+                return redirect('list')
+    context={
+        'my_form': my_form,
+    }
+    return render(request,'login.html',context)
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+
+def like(request, restaraunt_id):
+    restaraunt_obj = Restaurant.objects.get(id=restaraunt_id)
+
+    like_obj, created = FavourateRestaurant.objects.get_or_create(user=request.user, restaurant=restaraunt_obj)
+
+    if created:
+        action="like"
+    else:
+        action="unlike"
+        like_obj.delete()
+
+    like_count = restaraunt_obj.favouraterestaurant_set.all().count()
+    # like_count = Like.objects.filter(article=article_obj)
+
+    context = {
+        "action":action,
+        "count":like_count
+    }
+    return JsonResponse(context, safe=False)
+    
+
+
